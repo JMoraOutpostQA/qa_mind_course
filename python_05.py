b548d99679137c42d9645a0611394a8309e582d3
Juan Mora <jmora@outpostqa.com>
2023-02-16 18:57:28 -0700

    Practica python_05 Es par agregado

--- python_05.py
+++ python_05.py
@@ -16,6 +16,9 @@ def modulo(numeromod_a, numeromod_b):
 def num_int_to_float(entero):
     return float(entero)
 
+def num_float_to_int(flotante):
+    return int(flotante)
+
 def celsius_a_farenheit(celsiusf):
     return celsiusf * 1.8 + 32
 
@@ -28,6 +31,7 @@ def es_par(int):
 num_a = float(input("Ingresa el primer numero"))
 num_b = float(input("Ingresa el segundo numero:"))
 num_inte = int(input("Ingresa un numero entero"))
+num_float = float(input("Ingresa un numero flotante"))
 celcius = float(input("Ingresa los grados celcius:"))
 
 
@@ -37,6 +41,7 @@ print(f"Multiplicacion {multiplicacion(num_a, num_b)}")
 print(f"Division {division(num_a, num_b)}")
 print(f"Modulo {modulo(num_a, num_b)}")
 print(f"Numero entero a flotante {num_int_to_float(num_inte)}")
+print(f"Numero flotante a entero {num_float_to_int(num_float)}")
 print(f"Grados Farenheit {celsius_a_farenheit(celcius)}")
 print(f"Tu numero entero {es_par(num_inte)}")
 
